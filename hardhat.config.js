1357865209a1e5ec0fda2d9fffb933444414ef58
The Peregrn Protocol <tsatoshiearn@gmail.com>
2025-07-18 14:58:04 +0200

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+﻿require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { MEGAETH_RPC_URL, PRIVATE_KEY, MEGAETH_CHAIN_ID } = process.env;
+
+module.exports = {
+  solidity: {
+    version: "0.8.30",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+      debug: {
+        revertStrings: "strip", // Supprime les chaînes de reversion
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  networks: {
+    megaeth: {
+      url: MEGAETH_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: parseInt(MEGAETH_CHAIN_ID),
+    },
+  },
+};
