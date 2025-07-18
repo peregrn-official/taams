6132a84f5ce32baebd615d3bfd42909e7f462655
The Peregrn Protocol <tsatoshiearn@gmail.com>
2025-07-18 20:54:59 +0200

    go

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./taams/src/**/*.{html,js}",
-    "./index.html"
+    "./taams/src/**/*.{html,js}", // Chemin vers tes fichiers sources
+    "./index.html" // Fichier HTML racine
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
